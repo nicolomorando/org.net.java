6915e9fba60dcfd5a387bf13375656559e498fa8
Nicolò Morando <morandonic@gmail.com>
2018-10-12 09:47:11 +0200

    added first basic project

---
+++ org.datagram.client/dataClient.java
@@ -0,0 +1,19 @@
+﻿/**
+ * @author Morando Nicolò
+ * Datagram version 1.1
+ */
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class dataClient{
+  public static void main(String[] args) throws Exception {
+    DatagramSocket ds = new DatagramSocket(3000);
+    byte[] buf = new byte[516]; 
+    DatagramPacket dp = new DatagramPacket(buf, 516);
+    ds.receive(dp);   
+    String strRecv = new String(dp.getData(), 0, dp.getLength());
+    System.out.println("data received: " + "'" + strRecv + "'");
+    ds.close();
+  }
+}
---
+++ org.datagram.client/dataSender.java
@@ -0,0 +1,21 @@
+﻿/**
+ * @author Morando Nicolò
+ * Datagram version 1.1
+ */
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.io.File;
+
+public class dataSender{
+  public static void main(String[] args) throws Exception {
+    DatagramSocket ds = new DatagramSocket();
+    String str = "I'm here!!";
+    InetAddress ia = InetAddress.getByName("127.0.0.1");
+    DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ia, 3000);
+    System.out.println("Sended String: " + "'" + str + "'");
+    ds.send(dp);
+    ds.close();
+  }
+}
